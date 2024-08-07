214297c250af5cd738cf557bd859142cc7680702
VannWasHere <ateen1203@gmail.com>
2024-08-07 15:51:17 +0700

    Add login pages

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,8 +1,16 @@
+import Pocketbase from 'pocketbase';
+
 const App = () => {
+  const pb = new Pocketbase("http://127.0.0.1:8090")
+  const loginGoogle = async () => {
+    const response = await pb.collection("users").authWithOAuth2({provider: "google"})
+    console.log(response)
+  }
+
   return (
-    <>
-    <h1 className="text-2xl font-semibold">Test application</h1>
-    </>
+    <div className="">
+      <button className='p-2 w-1/3' onClick={loginGoogle}>Login Google</button>
+    </div>
   )
 }
 
---
+++ Frontend/src/pages/LoginPage.tsx
@@ -0,0 +1,9 @@
+const LoginPage = () => {
+    return (
+        <div className="bg-yellow-50">
+            
+        </div>
+    )
+}
+
+export default LoginPage;
