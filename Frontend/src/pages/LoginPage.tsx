fad1678a0e36903ad2046c6fbccc27404e04f9ba
VannWasHere <ateen1203@gmail.com>
2024-08-08 11:20:10 +0700

    Adding LoginPage

--- Frontend/src/pages/LoginPage.tsx
+++ Frontend/src/pages/LoginPage.tsx
@@ -1,7 +1,43 @@
+import Pocketbase from 'pocketbase';
+
 const LoginPage = () => {
+
+    const pb = new Pocketbase(import.meta.env.VITE_POCKETBASE)
+
+    const loginGoogle = async () => {
+        let response = null;
+        try {
+            response = await pb.collection("users").authWithOAuth2({provider: "google"})
+        } catch(err) {
+            console.error(err);
+        }
+        finally {
+            console.log(response);
+        }
+    }
+
     return (
-        <div className="bg-yellow-50">
-            
+        <div className="h-screen flex items-center justify-center">
+            <div className="bg-slate-100 text-black border border-white w-1/5">
+                <div className="w-full bg-black text-white p-6">
+                    <h1 className="text-2xl uppercase text-center">Login Form</h1>
+                </div>
+                <div className="w-full p-2">
+                    <p className="text-sm">Please fill out this form below!</p>
+                    <section className="mt-4">
+                        <div>
+                            <label htmlFor="Email">Email <span className="text-red-600">*</span></label>
+                            <input type="email" name="email" className="block p-1 w-full bg-transparent border-b-2 border-black focus:outline-none" />
+                        </div>
+                        <div className="mt-4">
+                            <label htmlFor="Password">Password <span className="text-red-600">*</span></label>
+                            <input type="password" name="password" className="block p-1 w-full bg-transparent border-b-2 border-black focus:outline-none" />
+                        </div>
+                        <button className="w-full p-2 bg-green-700 text-white mt-4">Submit</button>
+                        <button className="w-full p-2 bg-white font-semibold mt-4" onClick={loginGoogle}>Continue with google</button>
+                    </section>
+                </div>
+            </div>
         </div>
     )
 }
